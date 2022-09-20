57b9be21bffd7d94f060ee5e0e70eb246054eab2
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-20 22:50:48 +0700

    разница между максимальным и минимальным элементов массива

---
+++ Задача3/Program.cs
@@ -0,0 +1,25 @@
+﻿double[] arrayRealNumbers = new double[10];
+  for (int i = 0; i < arrayRealNumbers.Length; i++ )
+  {
+    arrayRealNumbers[i] = new Random().Next(1, 100);
+    Console.Write(arrayRealNumbers[i] + " ");
+  }
+
+double maxNumber = arrayRealNumbers[0];
+double minNumber = arrayRealNumbers[0];
+
+  for (int i = 1; i < arrayRealNumbers.Length; i++)
+  {
+    if (maxNumber < arrayRealNumbers[i])
+    {
+      maxNumber = arrayRealNumbers[i];
+    }
+        if (minNumber > arrayRealNumbers[i])
+    {
+      minNumber = arrayRealNumbers[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+
+  Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами: {decision}");
