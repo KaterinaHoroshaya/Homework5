b9a723eceea12879636e12dd64c7b6d65dec528d
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-20 22:48:22 +0700

    сумма элементов, стоящих на нечётных позициях

---
+++ Задача2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write($"Введи количество элементов массива: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+  return sumElements;
+  }
+
+int randomNumbers =  RandomNumbers(numberElements, 1, 100);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
